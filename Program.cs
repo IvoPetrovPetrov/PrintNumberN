6863df67e9d8fcb6cf55816ce71556c7b74269c9
Ivo Petrov <Ivo.Petrov@ascent.io>
2025-11-26 16:06:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PrintNumberN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number;
+            do
+            {
+                Console.WriteLine("Please enter a number between -1000 and 999:");
+                number = int.Parse(Console.ReadLine());
+            }
+            while (number < -1000.0 || number > 999.0);
+
+            for (int i = number; i <= 999; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
